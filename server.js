ea00ad018fce03bedd90b0c9f2d4ec94d7781fd6
Yuristy <yoosambee@gmail.com>
2021-03-20 09:51:53 +0300

    Added server with express, connected to socket.io

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const io = require('socket.io')(server);
+
+const rooms = new Map();
+
+app.get('/rooms', (req, res) => {
+    res.json(rooms);
+});
+
+io.on('connection', socket => {
+    console.log('socket connected', socket.id);
+})
+
+server.listen(9999, (err) => {
+    if (err) {
+        throw Error(err);
+    }
+    console.log('Сервер запущен!');
+});
