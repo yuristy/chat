d532b365384ff51e7936be244d74ab449cefee8b
Yuristy <yoosambee@gmail.com>
2021-03-23 20:29:34 +0300

    Made the component to work with hooks and axios.

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import socket from '../socket';
+import axios from 'axios';
 
 function JoinBlock() {
-    return (
-        <div className="join-block">
-            <input type="text" placeholder="ID комнаты"></input>
-            <input type="text" placeholder="Ваше имя"></input>
-            <button className="btn btn-success">ВОЙТИ</button>
-        </div>
-    )
+  const [roomId, setRoomId] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+
+  const onEnter = () => {
+    if (!roomId || !userName) {
+      return alert('Неверные данные');
+    }
+    axios.post('/rooms', {
+      roomId,
+      userName,
+    });
+  };
+
+  return (
+    <div className="join-block">
+      <input
+        type="text"
+        placeholder="ID комнаты"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={onEnter} className="btn btn-success">
+        ВОЙТИ
+      </button>
+    </div>
+  );
 }
 
 export default JoinBlock;
