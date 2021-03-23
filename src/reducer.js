e61c61b970a8b18c4b55682e23ad2ab91bea7cba
Yuristy <yoosambee@gmail.com>
2021-03-23 22:23:30 +0300

    Made separate reducer file for auth.

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+export default (state, action) => {
+  switch (action.type) {
+    case 'JOINED':
+      return {
+        ...state,
+        joined: action.payload,
+      };
+
+    default:
+      break;
+  }
+};
