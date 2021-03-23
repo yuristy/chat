0fdfaab44ff8aee39dcf04e05852a6ab04a6e6a6
Yuristy <yoosambee@gmail.com>
2021-03-23 20:25:37 +0300

    Dumped cors, added axios post method.

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 const express = require('express');
 
 const app = express();
+/*
+const cors = require('cors');
+*/
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-    cors:
-    {
-        origin: "*", methods: ["GET", "POST"],
-        credentials: true,
-        transports: ['websocket']
-    }
+const io = require('socket.io')(server);
+/* {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+    transports: ['websocket'],
+    optionsSuccessStatus: 200,
+  },
 });
+*/
+app.use(express.json());
 
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => {
-    res.json(rooms);
+  res.json(rooms);
+});
+
+app.post('/rooms', (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ['users', new Map()],
+        ['messages', []],
+      ])
+    );
+  }
+  res.send();
 });
 
 io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
-})
+  console.log('socket connected', socket.id);
+});
 
 server.listen(9999, (err) => {
-    if (err) {
-        throw Error(err);
-    }
-    console.log('Сервер запущен!');
+  if (err) {
+    throw Error(err);
+  }
+  console.log('Сервер запущен!');
 });
